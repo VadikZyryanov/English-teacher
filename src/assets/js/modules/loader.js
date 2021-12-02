039365f9bc55aeefbce01bb4209dee2f55057860
Vadik Zyryanov <vadik94_1rec@bk.ru>
2021-12-03 00:00:25 +0500

    fix comment

--- src/assets/js/modules/loader.js
+++ src/assets/js/modules/loader.js
@@ -1,8 +1,8 @@
-// try {
-//     const loader = document.querySelector('.loader');
+try {
+    const loader = document.querySelector('.loader');
 
-// setTimeout(() => {
-//     loader.classList.add('active');
-//     document.body.classList.remove('hidden');
-// }, 3900)
-// } catch {}
+setTimeout(() => {
+    loader.classList.add('active');
+    document.body.classList.remove('hidden');
+}, 3900)
+} catch {}
